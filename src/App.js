9f8332cccabf7ecccce3ce3193620c2561854dae
mrjoshdev <wyman.josh@gmail.com>
2019-01-22 15:42:43 -0500

    added Titles component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
-import Anchor_Man from './Anchor_Man.png';
 import './App.css';
+import Titles from './components/Titles';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Ron's Weather App</h1>
-          <br></br>
-          <img src={Anchor_Man} className="App-logo" alt="Anchor_Man" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <Titles />
+        </div>
       </div>
     );
   }
-}
+};
 
 export default App;
