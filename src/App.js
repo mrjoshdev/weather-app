7b35a48c89b8fc4bfee6841422cd1761b39b9c05
mrjoshdev <wyman.josh@gmail.com>
2020-05-29 15:52:04 -0400

    added icons to homepage

--- src/App.js
+++ src/App.js
@@ -16,13 +16,25 @@ class App extends Component {
     country: undefined,
     humidity: undefined,
     description: undefined,
+    icon: undefined,
     error: undefined
   }
+
+  /*this.icon = {
+    Thunderstorm: "wi-hunderstorm",
+    Drizzle: "wi-sleet",
+    Rain: "wi-storm-showers",
+    Snow: "wi-snow",
+    Atmosphere: "wi-fog",
+    Clear: "wi-day-sunny",
+    Clouds: "wi-day-fog"
+  };*/
 
   getWeather = async (e) => {
     e.preventDefault();
     const zipcode = e.target.elements.zipcode.value;
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},US&APPID=${REACT_APP_API_KEY}&units=imperial`)
+    const url = "http://openweathermap.org/img/wn/"
 
     const data = await api_call.json();
     if (zipcode && data["cod"] !== "404" ) {
@@ -32,6 +44,7 @@ class App extends Component {
       country: data.sys.country,
       humidity: data.main.humidity,
       description: data.weather[0].description,
+      icon: url + data.weather[0].icon + "@2x.png",
       error: ""
     });
   } else {
@@ -41,6 +54,7 @@ class App extends Component {
       country: undefined,
       humidity: undefined,
       description: undefined,
+      icon: url + "10d@2x.png",
       error: "Please enter a correct zipcode"
     })
   }
@@ -59,17 +73,32 @@ class App extends Component {
         </div>
         <body>
           <div class="row">
-            <div class="weather-block small-6 columns">
-            <span>
-                <Weather
-                temperature={this.state.temperature}
-                city={this.state.city}
-                country={this.state.country}
-                humidity={this.state.humidity}
-                description={this.state.description}
-                error={this.state.error}
-                />
-                </span>
+            <div class="weather-block small-6 large-6 columns">
+              <span>
+                  <Weather
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
+                  />
+              </span>
+            </div>
+            <div class="icon-block small-2 large-2 columns">
+              <Weather
+              icon={this.state.icon}
+              />
+            </div>
+            <div class="icon-block small-2 large-2 columns">
+              <Weather
+              icon={this.state.icon}
+              />
+            </div>
+            <div class="icon-block small-2 large-2 columns">
+              <Weather
+              icon={this.state.icon}
+              />
             </div>
           </div>
         </body>
