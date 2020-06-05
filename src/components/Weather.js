514573313d664affbec8bca68d1c593a97ee7abc
mrjoshdev <wyman.josh@gmail.com>
2020-06-05 10:10:13 -0400

    made background images dynamic

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../foundation.css';
 import '../App.css';
-import favicon from '../favicon.png';
 
 
 const Weather = (props) => {
     return(
       <div>
       {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
-      {props.temperature && <p>Temp: {Math.round(props.temperature)} F</p>}
+      {props.temperature && <p>Temp: {Math.round(props.temperature)} °F</p>}
       {props.humidity && <p>Humidity: {props.humidity}</p>}
       {props.description && <p>Conditions: {props.description}</p>}
-      {props.icon && <p>Icon: <img src={props.icon}/> </p>}
+      {props.icon && <p>Icon: <img src={props.icon} alt="icon"/> </p>}
+      {props.id && <p>Id: {props.id} </p>}
       {props.error && <p>{props.error}</p>}
       </div>
     );
