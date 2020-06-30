9910bec0ba0d45cd4658a8ff6ae5467acda99273
mrjoshdev <wyman.josh@gmail.com>
2020-06-30 12:12:41 -0400

    cleaned up components code, wrapped emoji in span

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Titles = () => {
     return (
       <div>
-        <h1 className='title-container__title'>Weather Party  🎉</h1>
+        <h1 className='title-container__title'>Weather Party <span role = 'img' aria-label="party-horn" description= 'party horn emoji'>🎉</span></h1>
         <p className='title-container__subtitle'>Enter US zipcode...</p>
       </div>
     );
